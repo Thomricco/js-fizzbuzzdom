7102568169bfb9bec6e7edffb03ed1f2948c4117
Thomricco <tommasoricco@hotmail.it>
2021-12-10 15:23:40 +0100

    finito ( da capire come scrivere % 5 e 3

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+const boxingHtml = document.querySelector(`.boxing`);
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 && 3 === 0 && i !== 0) {
+        // divisibile per 5 o 3
+        boxingHtml.innerHTML += '<div class="box box-bianco">FizBuz</div>'
+    }
+    else if (i % 5 === 0 && 1 !== 0) {
+        // divisibile per 5
+        boxingHtml.innerHTML += '<div class="box box-verde">Buz</div>'
+    }
+    else if (i % 3 === 0 && 1 !== 0) {
+        // divisibile per 3
+        boxingHtml.innerHTML += '<div class="box box-rosso">Fizz</div>'
+    }
+    else {
+        boxingHtml.innerHTML += `<div class="box">${i}</div>`
+    }
+} 
